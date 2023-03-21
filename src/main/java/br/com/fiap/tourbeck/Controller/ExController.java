15c3b5dfe4c17bf500d1227c2ed191a2c11ac6d5
brunoecp <rm95315@fiap.com.br>
2023-03-21 12:10:36 -0300

    att

--- src/main/java/br/com/fiap/tourbeck/Controller/ExController.java
+++ src/main/java/br/com/fiap/tourbeck/Controller/ExController.java
@@ -1,9 +1,18 @@
 package br.com.fiap.tourbeck.Controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.tourbeck.models.Despesa;
@@ -11,9 +20,54 @@ import br.com.fiap.tourbeck.models.Despesa;
 @RestController
 public class ExController {
     
+    Logger log = LoggerFactory.getLogger(ExController.class);
 
+    List<Despesa> despesas = new ArrayList<>();
+
+    //GET ALL
     @GetMapping("/api/v1/despesas")
-    public Despesa Home(){
-        return new Despesa(1l, new BigDecimal(100), LocalDate.now(), "Monkey");
+    public List<Despesa> Home(){
+        return despesas;
+    }
+    //GET
+    @GetMapping("api/v1/despesas/{id}")
+    public ResponseEntity<Despesa> show(@PathVariable Long id) {
+         var despesa = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
+
+         if ( despesa.isEmpty()) return ResponseEntity.notFound().build();
+         
+         return ResponseEntity.ok(despesa.get());
+    }
+    //POST 
+    @PostMapping("/api/v1/despesas")
+    public ResponseEntity<Despesa> create(@RequestBody Despesa despesa){
+        log.info("despesa cadastrada " + despesa);
+        despesas.add(despesa);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(despesa);
+    }
+
+    //DELETE
+    @DeleteMapping("/api/v1/despesas/{id}")
+    public ResponseEntity<Despesa> delete(@PathVariable Long id) {
+        var despesa = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
+
+        if ( despesa.isEmpty()) return ResponseEntity.notFound().build();
+
+        despesas.remove(despesa.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    //UPDATE
+    @PutMapping("/api/v1/despesas/{id}")
+    public ResponseEntity<Despesa> update(@PathVariable Long id, @RequestBody Despesa despesa) {
+        var despesaExistente = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
+
+        if ( despesaExistente.isEmpty()) return ResponseEntity.notFound().build();
+
+        despesa.setId(id);
+        despesas.remove(despesaExistente.get());
+        despesas.add(despesa);
+        return ResponseEntity.ok(despesa);
     }
 }
